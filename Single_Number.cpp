bb231359b0c3fb86d7f527fa4b38e2fe71cbda8a
Abhisar21 <21it3002@rgipt.ac.in>
2025-01-04 21:12:39 +0530

    Single Number

---
+++ Single_Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        
+        // XOR all numbers in the array
+        for(int num : nums) {
+            result ^= num;
+        }
+        
+        return result;
+    }
+};
