3067d6f0d1634bced3c73dee1323a7e2e50b3de9
Abhisar21 <21it3002@rgipt.ac.in>
2025-01-12 17:59:12 +0530

    Check if array is sorted and rotated

---
+++ Check_if_array_is_sorted_rotated.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    bool check(vector<int>& nums) {
+         int n = nums.size();
+    int count = 0; // Count of "rotational dips"
+
+    for (int i = 0; i < n; i++) {
+        // Check if the current element is greater than the next element
+        if (nums[i] > nums[(i + 1) % n]) {
+            count++;
+        }
+        // If there is more than one "rotational dip," it's not a valid rotated sorted array
+        if (count > 1) {
+            return false;
+        }
+    }
+
+    return true;
+    }
+
+};
